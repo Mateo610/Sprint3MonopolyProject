0a771f1c49560be5d2876df23a11d1d7502adb95
Deborah <rabinovichd@moravian.edu>
2025-04-05 12:26:16 -0400

    Part of board controller

--- src/Controller/BoardController.java
+++ src/Controller/BoardController.java
@@ -7,6 +7,84 @@
 
 package Controller;
 
+import Model.Board.Banker;
+import Model.Board.GameBoard;
+import Model.Board.Player;
+import Model.Exceptions.PlayerNotFoundException;
+import Model.Property.Property;
+
 public class BoardController {
+    private GameBoard gameBoard;
+    private Banker banker;
+
+    public BoardController() {
+        this.gameBoard = new GameBoard();
+        this.banker = gameBoard.getBanker();
+    }
+    public boolean buyHouse(Property property, Player player) {
+        try {
+            if (canBuyHouse(property, player)) {
+                banker.sellHouse(property, player);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PlayerNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public boolean buyHotel(Property property, Player player) {
+        try {
+            if (canBuyHotel(property, player)) {
+                banker.sellHotel(property, player);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PlayerNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public boolean sellHouse(Property property, Player player) {
+        try {
+            if (canSellHouse(property, player)) {
+                banker.buyBackHouse(property, player);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PlayerNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public boolean sellHotel(Property property, Player player) {
+        try {
+            if (canSellHotel(property, player)) {
+                banker.buyBackHotel(property, player);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PlayerNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean canBuyHouse(Property property, Player player) {
+        return false;
+    }
+    private boolean canBuyHotel(Property property, Player player) {
+        if (property.getOwner() != player) {
+            return false;
+        }
+
+    }
+
+    private boolean canSellHotel(Property property, Player player) {
+    }
+
+    private boolean canSellHouse(Property property, Player player) {
+    }
+
 
 }
